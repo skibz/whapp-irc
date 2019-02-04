90c71364fe347846cadea113ac3bbfda76111ed7
Ant Cosentino <antony@mycosentino.com>
2019-02-04 13:30:58 +0200

    add an init function to set up acquired resources that are used in main, fixes circular dependency by removing log level shit, conditionally asynchronously hooks upstream for irc connect, marshalls config as pointer for called functions, if operating in driven mode the tcp server stops accepting new connections once the first client has connected

--- main.go
+++ main.go
@@ -1,48 +1,95 @@
 package main
 
 import (
+	"errors"
 	"log"
 	"net"
+	"net/http"
+	"net/url"
+	"strings"
 	"time"
 	"whapp-irc/config"
 	"whapp-irc/database"
 	"whapp-irc/files"
 	"whapp-irc/maps"
-	"whapp-irc/whapp"
 
 	"github.com/chromedp/chromedp"
 )
 
 var (
+	cfg    *config.Config
 	fs     *files.FileServer
 	userDb *database.Database
 	pool   *chromedp.Pool
 
-	loggingLevel      whapp.LoggingLevel
+	listener *net.TCPListener
+
 	mapProvider       maps.Provider
 	alternativeReplay bool
 
 	startTime = time.Now()
 	commit    string
+
+	upstreamDriver = false
 )
 
-func makePool(loggingLevel whapp.LoggingLevel) (*chromedp.Pool, error) {
-	switch loggingLevel {
-	case whapp.LogLevelVerbose:
-		return chromedp.NewPool(chromedp.PoolLog(log.Printf, log.Printf, log.Printf))
-	default:
-		return chromedp.NewPool()
+func triggerUpstreamIrcConnect() {
+	errfmt := "upstream irc server connect error: %e"
+	uri, err := url.ParseRequestURI(cfg.UpstreamIRCBaseURI)
+	if err != nil {
+		log.Printf(errfmt, err)
+		return
 	}
+
+	uri.Path = cfg.UpstreamIRCPath
+	body := url.Values{}
+
+	for _, channel := range cfg.IRCChannels {
+		body.Add("chan", channel)
+	}
+
+	body.Add("host", cfg.Hostname)
+	body.Add("nick", cfg.IRCNickname)
+	body.Add("id", cfg.IRCIdentityID)
+	body.Add("hash", cfg.IRCIdentityHash)
+
+	reader := strings.NewReader(body.Encode())
+	req, err := http.NewRequest(cfg.UpstreamIRCMethod, uri.String(), reader)
+	if err != nil {
+		log.Printf(errfmt, err)
+		return
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	c := &http.Client{}
+	res, err := c.Do(req)
+	if err != nil {
+		log.Printf(errfmt, err)
+		return
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode >= 400 {
+		log.Printf(errfmt, errors.New("upstream sent "+res.Status))
+		return
+	}
+
+	return
 }
 
-func main() {
+func init() {
+	var err error
+
 	config, err := config.ReadEnvVars()
 	if err != nil {
 		panic(err)
 	}
-	loggingLevel = config.LoggingLevel
-	mapProvider = config.MapProvider
-	alternativeReplay = config.AlternativeReplay
+	cfg = &config
+
+	mapProvider = cfg.MapProvider
+	alternativeReplay = cfg.AlternativeReplay
 
 	userDb, err = database.MakeDatabase("db/users")
 	if err != nil {
@@ -50,48 +97,92 @@ func main() {
 	}
 
 	fs, err = files.MakeFileServer(
-		config.FileServerHost,
-		config.FileServerPort,
+		cfg.FileServerHost,
+		cfg.FileServerPort,
 		"files",
-		config.FileServerHTTPS,
+		cfg.FileServerHTTPS,
 	)
 	if err != nil {
 		panic(err)
 	}
+	pool, err = chromedp.NewPool()
+	if err != nil {
+		panic(err)
+	}
+
+	addr, err := net.ResolveTCPAddr("tcp", ":"+cfg.IRCPort)
+	if err != nil {
+		panic(err)
+	}
+
+	listener, err = net.ListenTCP("tcp", addr)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+
+	// boot the file server
 	go func() {
+
+		// FIXME
+		// crash parent on error
+
 		if err := fs.Start(); err != nil {
 			log.Printf("error while starting fileserver: %s", err)
 		}
 	}()
-	defer fs.Stop()
 
-	pool, err = makePool(loggingLevel)
-	if err != nil {
-		panic(err)
-	}
-	defer pool.Shutdown()
+	// ensure the file server shuts down
+	// ensure the chrome pool shuts down
+	// ensure the tcp server shuts down
+	// FIXME
+	// escalate handle any errors (not that they really matter)
+	defer func() {
+		listener.Close()
+		fs.Stop()
+		pool.Shutdown()
+	}()
 
-	addr, err := net.ResolveTCPAddr("tcp", ":"+config.IRCPort)
-	if err != nil {
-		panic(err)
-	}
-
-	listener, err := net.ListenTCP("tcp", addr)
-	if err != nil {
-		panic(err)
+	if cfg.UpstreamIRC {
+		go triggerUpstreamIrcConnect()
 	}
 
 	for {
+		if upstreamDriver {
+			break
+		}
+		// try and init a new connection
 		socket, err := listener.AcceptTCP()
+
+		// FIXME
+		// crash parent on error, rather?
 		if err != nil {
 			log.Printf("error accepting TCP connection: %s", err)
 			continue
 		}
 
+		// and asynchronously do irc
+		// FIXME
+		// crash parent on error, rather?
 		go func() {
-			if err := BindSocket(socket); err != nil {
+			if err := BindSocket(socket, cfg); err != nil {
 				log.Println(err)
 			}
 		}()
+
+		// update the exit condition variable
+		upstreamDriver = cfg.UpstreamIRC && true
+	}
+
+	if cfg.UpstreamIRC {
+		// stop accepting new connections
+		// now that upstream has connected
+		listener.Close()
+
+		for {
+			// and wait until something crashes
+		}
 	}
 }
